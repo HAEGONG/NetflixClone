c715a9c4ffd366d23876dc1063a5a05ff0c502f3
HAEGONG <dgr0725@gmail.com>
2022-03-26 21:04:47 +0900

    add upcoming view

---
+++ NetflixClone/ViewModels/TitleViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  TitleViewModel.swift
+//  NetflixClone
+//
+//  Created by 이해공 on 2022/03/26.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+    
+}
